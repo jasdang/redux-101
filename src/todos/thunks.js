18fb80979151daf44c336231eba13cf238757360
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-17 23:26:13 +0800

    added async thunks

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,22 @@
-export const displayAlert = () => {
-  alert('Hello!');
+import {
+  loadTodosInProgress,
+  loadTodosSuccess,
+  loadTodosFailure,
+} from './actions';
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch('http://localhost:8080/todos');
+    const todos = await response.json();
+
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+};
+
+export const displayAlert = (text) => () => {
+  alert(text);
 };
