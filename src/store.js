03847d3bbc4b2e9e64ff4374297c2dbc0a72e4fa
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-17 23:10:01 +0800

    added a simple thunk displayAlert

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtool-extension';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {todos} from './todos/reducers';
 
 const reducers = {todos};
