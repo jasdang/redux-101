3540a68acb99f8e834a1087a785831853f55c20a
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-26 22:08:02 +0800

    added getCompleteTodos selector

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -5,3 +5,6 @@ export const getTodosLoading = (state) => state.todos.isLoading;
 export const getIncompleteTodos = createSelector(getTodos, (todos) =>
   todos.filter((todo) => !todo.isCompleted)
 );
+export const getCompletedTodos = createSelector(getTodos, (todos) =>
+  todos.filter((todo) => todo.isCompleted)
+);
