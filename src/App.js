7c14fe8836e8023e22ab625ca22b392e5f4885a3
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-27 23:43:20 +0800

    styled-component App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 import TodoList from './todos/TodoList';
+import styled from 'styled-components';
 
+const AppContainer = styled.div`
+  color: #222222;
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 1rem;
+  width: 100vw;
+  height: 100vh;
+`;
 const App = () => {
   return (
-    <div className='App'>
+    <AppContainer>
       <TodoList />
-    </div>
+    </AppContainer>
   );
 };
 
